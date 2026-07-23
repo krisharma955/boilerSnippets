4a4636bdca43b72e71a6d80f775ae87f36bb3979
krishsharma955 <krishsharma9005@gmail.com>
2026-07-23 13:46:40 +0530

    backend setup

---
+++ boilerSnippets-be/src/main/java/com/K955/boilerSnippets/BoilerSnippetsApplication.java
@@ -0,0 +1,13 @@
+package com.K955.boilerSnippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerSnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerSnippetsApplication.class, args);
+	}
+
+}
---
+++ boilerSnippets-be/src/test/java/com/K955/boilerSnippets/BoilerSnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.K955.boilerSnippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerSnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
